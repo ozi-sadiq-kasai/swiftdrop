359d2249528dd6172990288ae8739ea331aaa2ab
ozi <ozisadiqkasai@gmail.com>
2023-12-28 22:15:26 +0100

    modified files

--- src/component/Network.jsx
+++ src/component/Network.jsx
@@ -1,6 +1,18 @@
-import { FaArrowDown } from 'react-icons/fa6'
+import { FaArrowDown, FaArrowRight } from 'react-icons/fa6'
+import { LuShoppingBag } from 'react-icons/lu'
+import { FaBicycle } from 'react-icons/fa'
+import { GiLaptop } from 'react-icons/gi'
+import { Link } from 'react-router-dom'
+import shoppingBags from '../assets/images/shoppingbags.png'
+import deliverybike from '../assets/images/deliverybike.png'
+import remotework from '../assets/images/remotework.png'
+import 'animate.css'
+
+
 
 const Network = () => {
+
+
   return (
     <div className="relative top-28">
       <div className="flex justify-center items-center gap-3">
@@ -9,7 +21,58 @@ const Network = () => {
         </h1>
         <FaArrowDown className="text-2xl font text-gray-300" />
       </div>
+      <div>
+        {/*  */}
+        <div className="bg-[#FFFBCE] mx-5 my-7 py-5 px-3 shadow-lg rounded">
+          <div className="flex items-center gap-2">
+            <LuShoppingBag className="text-[#2FC864] text-xl" />
+            <p className="text-xl font-semibold">Start Selling</p>
+          </div>
+          <p className="leading-7 text-md pt-3">
+            Are you a business owner looking to grow your business? Reach new
+            customers when you join our network.
+          </p>
+          <div className="flex items-center gap-2">
+            <Link to="/">See More </Link>
+            <FaArrowRight />
+          </div>
+          <img src={shoppingBags} alt="shoppingBags" />
+        </div>
+        {/*  */}
+        <div className="bg-[#FFFBCE] mx-5 my-7 py-5 px-3 shadow-lg rounded">
+          <div className="flex items-center gap-2">
+            <FaBicycle className="text-[#2FC864] text-xl" />
+            <p className="text-xl font-semibold">Deliver happiness</p>
+          </div>
+          <p className="leading-7 text-md pt-3">
+            Join our elite league of delivery riders delivering happiness to
+            customers and earn to achieve your dreams while at it.
+          </p>
+          <div className="flex items-center gap-2">
+            <Link to="/">See More </Link>
+            <FaArrowRight />
+          </div>
+          <img src={deliverybike} alt="deliverybike" />
+        </div>
+        {/*  */}
+        <div className="bg-[#FFFBCE] mx-5 my-7 py-5 px-3 shadow-lg rounded mb-64">
+          <div className="flex items-center gap-2">
+            <GiLaptop className="text-[#2FC864] text-xl" />
+            <p className="text-xl font-semibold">Behind the scenes</p>
+          </div>
+          <p className="leading-7 text-md pt-3">
+            If you are passionate about helping us achieve our goal to deliver
+            meals seamlessly, come join the team.
+          </p>
+          <div className="flex items-center gap-2">
+            <Link to="/">See More </Link>
+            <FaArrowRight />
+          </div>
+          <img src={remotework} alt="remotework" />
+        </div>
+      </div>
     </div>
   )
 }
 export default Network
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,32 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+   
   ],
   theme: {
     extend: {
-  animation: {
-    ["infinite-slider"]: "infiniteSlider 20s linear infinite",
-  },
-  keyframes: {
-    infiniteSlider: {
-      "0%": { transform: "translateX(0)" },
-      "100%": {
-        transform: "translateX(calc(-250px * 5))",
+      animation: {
+        "infinite-slider": "infiniteSlider 20s linear infinite",
+      },
+      keyframes: {
+        infiniteSlider: {
+          "0%": { transform: "translateX(0)" },
+          "100%": {
+            transform: "translateX(calc(-250px * 5))",
+          },
+        },
+      },
+      screens: {
+        xsm: '414px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1440px',
       },
     },
   },
-    },
-  },
   plugins: [],
-}
+
+};
